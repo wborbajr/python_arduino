c1b93929b51f1e7ebcfebd09c59e0cf8299593ee
Waldir Borba Junior <wborbajr@gmail.com>
2019-02-17 20:35:06 -0300

    up

---
+++ blick.py
@@ -0,0 +1,29 @@
+try:
+  from pyfirmata import Arduino, util
+except:
+  import pip
+  pip.main([ 'install','pyfirmata'])
+  from pyfirmata import Arduino, util
+
+
+from pyfirmata import ArduinoMega, util
+from time import sleep
+
+port = port = '/dev/ttyACM0'
+board = ArduinoMega(port)
+
+print('Code is running')
+
+led = board.get_pin('d:13:o')
+
+for i in range(10):
+#  board.digital[13].write(0)
+  led.write(0)
+  sleep(1)
+  led.write(1)
+#  board.digital[13].write(1)
+  sleep(1)
+led.write(0)
+board.exit()
+print('End')
+
